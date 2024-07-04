8d9ef3bc04cf27c0a76cb58d99a39b8bff26425e
Nahid Islam <nahidcrx@outlook.com>
2024-07-05 01:49:02 +0600

    http client added and first request to api done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SkiNet';
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get<IPagination>("https://localhost:44310/api/Products?PageSize=50").subscribe(
+      (response: IPagination) =>
+        {
+          this.products = response.data;
+        }, error=> {
+          console.log(error);
+        }
+    );
+  }
 }
